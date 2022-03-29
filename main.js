ed6d3fefa95ffa48c488303a4f050447a2770501
dariszka <darinapetrova05@gmail.com>
2022-03-29 12:29:35 +0200

    Write game:D

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-let  pick = ["Rock", "Paper", "Scissors"]
+let  pick = ["rock", "paper", "scissors"]
 
 let computerPlay = pick[Math.floor(Math.random() * pick.length)];
 
-console.log(computerPlay)
+let playerPlay = prompt("?");
+
+let computerSelection = computerPlay
+let playerSelection = playerPlay.toLowerCase();
+
+console.log(playerSelection);
+console.log(computerSelection);
+
+function game() {
+    if (playerSelection === computerSelection)  {
+        return "remis nwm jak jest po angielsku"
+    } else if (playerSelection === "rock" && computerSelection === "paper")  {
+        return "u loose, rock<<paper babez"
+    } else if (playerSelection === "paper" && computerSelection === "scissors")  {
+        return "u loose, paper<<scissors babez"
+    } else if (playerSelection === "scissors" && computerSelection === "rock")  {
+        return "u loose, scrissors<<rock babez"
+    } else if (playerSelection === "paper" && computerSelection === "rock")  {
+        return "u win, paper>>rock babez"
+    }else if (playerSelection === "scissors" && computerSelection === "paper")  {
+        return "u win, scissors>>paper babez"
+    }else if (playerSelection === "rock" && computerSelection === "scissors")  {
+        return "u win, rock>>scissors babez"
+    }else {
+        return"hoe type a valid word pls"
+    }
+}
+
+console.log(game())
+
