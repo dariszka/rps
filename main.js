061f003a7189af068f9b86bb0bbefa5efae7225a
dariszka <darinapetrova05@gmail.com>
2022-03-30 16:10:11 +0200

    Ugly.

--- main.js
+++ main.js
@@ -23,3 +23,49 @@ function playRound() {
     }
 }
 
+//console.log(playRound())
+
+//turn score of 1 round into variable
+let roundScore = playRound()
+
+console.log(roundScore)
+
+//write score of one round  
+function score() {
+    if (roundScore === "remis nwm jak jest po angielsku" || roundScore === "hoe type a valid word pls") {
+        return undefined;
+    } else if (roundScore ==="u loose") {
+        return 0; 
+    } else {
+        return 1;
+    }
+    
+}
+console.log(score())
+
+score()
+let counter = score()
+
+
+
+
+//play rounds and count win if >2
+
+function game() {
+    for (let counter = 0; counter < 5; countScore++) {
+        if (counter === undefined) {
+            continue; 
+        } else {
+            return("whore")
+        }
+            
+    
+     }
+}
+
+
+game()
+console.log(game())
+
+//return "u loose whole round sucks 4u lmao"
+//return "u win whole round good job sport"
