e17fb3a6204b50f6e16308d869f58be22ae95f37
dariszka <darinapetrova05@gmail.com>
2022-03-30 15:53:22 +0200

    Write new game.

--- main.js
+++ main.js
@@ -2,33 +2,24 @@ let  pick = ["rock", "paper", "scissors"]
 
 let computerPlay = pick[Math.floor(Math.random() * pick.length)];
 
-let playerPlay = prompt("?");
+
 
 let computerSelection = computerPlay
-let playerSelection = playerPlay.toLowerCase();
+let playerSelection = prompt("?").toLowerCase();
 
 console.log(playerSelection);
 console.log(computerSelection);
 
-function game() {
+
+function playRound() {
     if (playerSelection === computerSelection)  {
         return "remis nwm jak jest po angielsku"
-    } else if (playerSelection === "rock" && computerSelection === "paper")  {
-        return "u loose, rock<<paper babez"
-    } else if (playerSelection === "paper" && computerSelection === "scissors")  {
-        return "u loose, paper<<scissors babez"
-    } else if (playerSelection === "scissors" && computerSelection === "rock")  {
-        return "u loose, scrissors<<rock babez"
-    } else if (playerSelection === "paper" && computerSelection === "rock")  {
-        return "u win, paper>>rock babez"
-    }else if (playerSelection === "scissors" && computerSelection === "paper")  {
-        return "u win, scissors>>paper babez"
-    }else if (playerSelection === "rock" && computerSelection === "scissors")  {
-        return "u win, rock>>scissors babez"
-    }else {
-        return"hoe type a valid word pls"
+    } else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
+        return "u loose"
+    } else if ((playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "rock" && computerSelection === "scissors")) {
+        return "u win"
+    } else {
+        return "hoe type a valid word pls"
     }
 }
-
-console.log(game())
 
